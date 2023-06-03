377a47aade12d45879081ce58cbe68808515f68f
Rahul Sharma <raulshma@gmail.com>
2023-06-03 22:32:24 +0530

    Fixed service resolution issue for dbcontext. (#3)

---
+++ backend/src/ThreeTee.Application/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ThreeTee.Application.Interfaces;
+using ThreeTee.Application.Services;
+
+namespace ThreeTee.Application.Extension;
+
+public static class Extension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<IBillingTypeService, BillingTypeService>();
+        return services;
+    }
+}
