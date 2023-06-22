e84acbec67ca0dc9d7f8fbaa03ece39497352af2
raulshma <raulshma@gmail.com>
2023-06-23 01:43:13 +0530

    User exp years calc.

--- backend/src/ThreeTee.Application/Models/ProjectUser/ProjectUserResponse.cs
+++ backend/src/ThreeTee.Application/Models/ProjectUser/ProjectUserResponse.cs
@@ -3,7 +3,24 @@ namespace ThreeTee.Application.Models.ProjectUser;
 
 public class ProjectUserResponse
 {
+    public string UserName { get; set; }
+    public string UserEmail { get; set; }
+    public string? UserDesignationName { get; set; }
+    public DateTime? UserJoiningDate { get; set; }
+    public int? UserExperienceYears
+    {
+        get
+        {
+            if (UserJoiningDate.HasValue)
+            {
+                DateTime zeroTime = new(1, 1, 1);
+                TimeSpan span = (TimeSpan)(DateTime.UtcNow - UserJoiningDate);
+                int years = (zeroTime + span).Year - 1;
+                return years;
+            }
+            return null;
+        }
+    }
     public Guid UserId { get; set; }
     public Guid ProjectId { get; set; }
 }
-
